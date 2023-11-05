6aa2fbfa19d778ef8ad022fb74a57451da9aec27
iibrahim70 <aviknuhash70@gmail.com>
2023-11-05 23:38:18 +0600

    added blogs get api

--- controllers/blogs.controllers.js
+++ controllers/blogs.controllers.js
@@ -5,4 +5,9 @@ const createBlogs = async (req, res) => {
   res.send(result);
 };
 
-module.exports = { createBlogs };
+const getAllBlogs = async (req, res) => {
+  const result = await blogModels.find().sort({ createdAt: -1 });
+  res.send(result);
+};
+
+module.exports = { createBlogs, getAllBlogs };
--- routes/blogs.routes.js
+++ routes/blogs.routes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const { verifyJWT, verifyAdmin } = require("../middlewares/auth.middleware");
-const { createBlogs } = require("../controllers/blogs.controllers");
+const {
+  createBlogs,
+  getAllBlogs,
+} = require("../controllers/blogs.controllers");
 
 const blogsRouter = express.Router();
 
 // admin routes
 blogsRouter.post("/", verifyJWT, verifyAdmin, createBlogs);
+
+// user routes
+blogsRouter.get("/", getAllBlogs);
 
 module.exports = blogsRouter;
